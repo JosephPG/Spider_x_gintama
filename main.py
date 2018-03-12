eee6219a00a7b4ef20d86c5d8941209fa4abaa41
JosephPG <saiborack@gmail.com>
2018-03-11 19:26:25 -0500

    Gintama Links

---
+++ main.py
@@ -0,0 +1,52 @@
+from urllib import request
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import re
+
+class GintamaLinks():
+
+    _data = None
+    _links = []
+    _mega = {}
+    
+    def __init__(self, url):
+        self.url = url
+
+    def connect(self):
+        try:
+            _request = request.Request(self.url)
+            _response = request.urlopen(_request)
+            self._data = _response.read()
+            return True
+        except HTTPError as error:
+            print(error)
+            return False
+
+    def get_ahref(self):
+        try:
+            _soup = BeautifulSoup(self._data, 'lxml')
+            _soup.prettify()           
+            for a in _soup.find_all(href=re.compile("mega")):
+                self._links.append(str(a))
+                
+        except Exception as error:
+            print(error)
+
+    def get_links(self):
+        for x in range(len(self._links)):
+            pos = self._links[x].find("mega.nz/")
+            url = self._links[x][pos:]
+            pos2 = url.find('">https')
+            url = url[:pos2]
+            url = url.replace("~~4dfl7SUCKS~~", "#")
+            self._mega[x+2] = url
+        [print("{} {}".format(x, self._mega[x])) for x in self._mega]
+            
+
+
+spider = GintamaLinks("http://foro.unionfansub.com/showthread.php?tid=11457")
+response = spider.connect()
+if response is True:
+    spider.get_ahref()
+    spider.get_links()
+        
